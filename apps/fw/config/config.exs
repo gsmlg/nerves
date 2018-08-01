32b6921ad06f864a764cc76c25dbd597ff6dc1bc
gsmlg <me@gsmlg.org>
2018-08-02 01:09:55 +0800

    update

--- apps/fw/config/config.exs
+++ apps/fw/config/config.exs
@@ -24,13 +24,13 @@ config :nerves_network, :default,
   wlan0: [
     ssid: System.get_env("NERVES_NETWORK_SSID"),
     psk: System.get_env("NERVES_NETWORK_PSK"),
-    key_mgmt: String.to_atom(System.get_env("NERVES_NETWORK_MGMT") || "")
+    key_mgmt: String.to_atom(System.get_env("NERVES_NETWORK_MGMT") || "WPA-PSK")
   ]
 
 # For Phoenix 
 config :ui, UiWeb.Endpoint,
   url: [host: "localhost"],
-  http: [port: 8080],
+  http: [port: 80],
   secret_key_base: "#############################",
   root: Path.dirname(__DIR__),
   server: true,
